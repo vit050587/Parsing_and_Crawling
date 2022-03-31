b0bcdcfceab173f342cf447bcd1b793910d2654e
Vitaly <vit050587@mail.ru>
2022-03-31 17:03:11 +0300

    Добавлено решение к первой задаче урока 1

--- lesson01.py
+++ lesson01.py
@@ -2,7 +2,32 @@
 # разобраться как вывести список наименований репозиториев для конкретного пользователя,
 # сохранить JSON-вывод в файле *.json.
 
+import requests
+import time
+import json
+from pprint import pprint
 
+def get_data(url: str) -> dict:
+    while True:
+        time.sleep(1)
+        response = requests.get(url)
+        if response.status_code == 200:
+            break
+    return response.json()
+
+username = input('Введите username: ')
+username = 'vit050587' if username == '' else username
+url = 'https://api.github.com/users/'+username+'/repos'
+
+response = get_data(url)
+repo = []
+for itm in response:
+    repo.append(itm['name'])
+print(f'Список репозиториев пользователя {username}\n', '*' * 50)
+pprint(repo)
+
+with open('1_1_repo.json', 'w') as f:
+    json_repo = json.dump(repo, f)
 
 
 
