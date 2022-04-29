27a75ac7e1ccc576377d23ceb3c7b9970498ca1d
Vitaly <vit050587@mail.ru>
2022-04-29 21:29:55 +0300

    runner

--- instaparser/pipelines.py
+++ instaparser/pipelines.py
@@ -5,9 +5,16 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
 
+from itemadapter import ItemAdapter
+from pymongo import MongoClient
 
 class InstaparserPipeline:
+    def __init__(self):
+        client = MongoClient('localhost', 27017)
+        self.mongobase = client.intagramFollow
+
     def process_item(self, item, spider):
+        collection = self.mongobase[item.get('username')]
+        collection.insert_one(item)
         return item
